d57e8c4c600444d60bb4c0ff9ea397745ba7f661
AJ Steers <aaronsteers@gmail.com>
2021-05-03 22:57:58 -0700

    infra module updates

--- meltano/infra/dev/meltano.tf
+++ meltano/infra/dev/meltano.tf
@@ -10,14 +10,19 @@ module "meltano" {
   # TODO: Revert to git source after testing:
   # source             = "git::https://github.com/dataops-tk/dataops-infra.git//catalog/aws/meltano?ref=feature/meltano-module"
 
-  environment          = module.aws_environ.environment
+  environment          = module.env.environment
   name_prefix          = local.name_prefix
   resource_tags        = local.resource_tags
-  project_source       = "git+https://${local.meltano_project_repo}.git"
+  project_source       = local.meltano_project_repo
+
+  # Note: This may need to be updated to account for daylight savings
+  tz_utc_offset        = -6
 
   # TODO: Remove
-  taps = {}
   data_lake_metadata_path = null
+  meltano_yml_path        = "../../../meltano.yml"
+  default_target          = "target-athena"
+
   # env                  = {
   #   GIT_URL = "git+https://gitlab.com/meltano/singerhub"
   #   GIT_PATH = "meltano-project"
