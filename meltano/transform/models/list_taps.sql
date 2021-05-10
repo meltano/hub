5fe9c1674498770aa6754ea96d8846c3f0c5798d
AJ Steers <aaronsteers@gmail.com>
2021-05-10 11:47:03 -0700

    fixed dedupe logic

--- meltano/transform/models/list_taps.sql
+++ meltano/transform/models/list_taps.sql
@@ -1,11 +1,17 @@
 SELECT
-    t.id            AS repo_id,
-    t.name,
-    t.full_name,
-    row_number() OVER (
-        PARTITION BY full_name ORDER BY _sdc_batched_at DESC
-    )               AS recency_rank
-FROM {{ source('tap_github_index', 'tap_something') }} as t
-WHERE t.name like 'tap-%'
-   OR t.name like '%-tap-%'
-  AND t.name not like 'tap-practica-%'
+    repo_id,
+    name,
+    full_name
+FROM (
+    SELECT
+        t.id            AS repo_id,
+        t.name,
+        t.full_name,
+        row_number() OVER (
+            PARTITION BY full_name ORDER BY _sdc_batched_at DESC
+        )               AS recency_rank
+    FROM {{ source('tap_github_index', 'tap_something') }} as t
+    WHERE (t.name like 'tap-%' OR t.name like '%-tap-%')
+      AND t.name not like 'tap-practica-%'
+) list_taps
+WHERE recency_rank = 1
--- meltano/transform/models/list_targets.sql
+++ meltano/transform/models/list_targets.sql
@@ -1,10 +1,17 @@
 SELECT
-    t.id          AS repo_id,
-    t.name,
-    t.full_name,
-    row_number() OVER (
-        PARTITION BY full_name ORDER BY _sdc_batched_at DESC
-    )               AS recency_rank
-FROM {{ source('tap_github_index', 'target_something') }} as t
-WHERE t.name like 'target-%'
-   OR t.name like '%-target-%'
+    repo_id,
+    name,
+    full_name
+FROM (
+    SELECT
+        t.id          AS repo_id,
+        t.name,
+        t.full_name,
+        row_number() OVER (
+            PARTITION BY full_name ORDER BY _sdc_batched_at DESC
+        )               AS recency_rank
+    FROM {{ source('tap_github_index', 'target_something') }} as t
+    WHERE t.name like 'target-%'
+    OR t.name like '%-target-%'
+) AS list_targets
+WHERE recency_rank = 1
