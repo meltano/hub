a10b83adbd65497a46a40662406dce7f0e1792f2
AJ Steers <aaronsteers@gmail.com>
2021-05-10 11:29:31 -0700

    remove dupes in dbt tables

--- meltano/transform/models/list_taps.sql
+++ meltano/transform/models/list_taps.sql
@@ -1,7 +1,11 @@
 SELECT
-    t.id          AS repo_id,
+    t.id            AS repo_id,
     t.name,
-    t.full_name
+    t.full_name,
+    row_number() OVER (
+        PARTITION BY full_name ORDER BY _sdc_batched_at DESC
+    )               AS recency_rank
 FROM {{ source('tap_github_index', 'tap_something') }} as t
 WHERE t.name like 'tap-%'
    OR t.name like '%-tap-%'
+  AND t.name not like 'tap-practica-%'
--- meltano/transform/models/list_targets.sql
+++ meltano/transform/models/list_targets.sql
@@ -1,7 +1,10 @@
 SELECT
     t.id          AS repo_id,
     t.name,
-    t.full_name
+    t.full_name,
+    row_number() OVER (
+        PARTITION BY full_name ORDER BY _sdc_batched_at DESC
+    )               AS recency_rank
 FROM {{ source('tap_github_index', 'target_something') }} as t
 WHERE t.name like 'target-%'
    OR t.name like '%-target-%'
