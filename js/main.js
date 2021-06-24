4f24e0b1c5878b2c35264c0e479d0f8c3777f0a2
Douwe Maan <douwe@selenight.nl>
2021-06-24 18:40:29 -0500

    Add search bar to plugin pages

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function escapeRegex(string) {
+  return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, "\\$&");
+}
+
+window.addEventListener("load", () => {
+  var field = document.querySelector("input.grid-search");
+  if (!field) {
+    return;
+  }
+
+  var grid = document.querySelector("ul.button-grid");
+  var items = grid.querySelectorAll("li[data-search-terms]");
+
+  field.addEventListener("input", (e) => {
+    var query = e.target.value.trim().toLowerCase();
+
+    items.forEach((item) => {
+      var terms = item.getAttribute("data-search-terms");
+
+      if (!query || terms.toLowerCase().indexOf(query) > -1) {
+        item.style.display = "block";
+      } else {
+        item.style.display = "none";
+      }
+    });
+  });
+
+  field.disabled = false;
+});
