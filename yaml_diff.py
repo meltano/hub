02ef89d448e333df6155172330605df6d5b6f744
Taylor A. Murphy <taylor@meltano.com>
2021-09-22 15:51:47 -0500

    Convert yaml_diff to Ruamel

--- yaml_diff.py
+++ yaml_diff.py
@@ -1,16 +1,18 @@
-import yaml
 import sys 
 
+from ruamel.yaml import YAML
 from deepdiff import DeepDiff
 from deepdiff.helper import CannotCompare
 
 from pprint import pprint
 
+yaml = YAML()
+
 with open("meltano.yml", "r") as meltano_file:
-    meltano = yaml.load(meltano_file, Loader=yaml.FullLoader)
+    meltano = yaml.load(meltano_file)
 
 with open("discovery.yml", "r") as hub_file:
-    hub = yaml.load(hub_file, Loader=yaml.FullLoader)
+    hub = yaml.load(hub_file)
 
 def compare_func(x, y):
     try:
@@ -29,10 +31,10 @@ ddiff = DeepDiff(
         iterable_compare_func=compare_func
     )
 
-if ddiff == "{}":
+if len(ddiff) == 0:
     print("Generated discovery.yml matches Meltano discovery.yml")
     sys.exit()
 else:
-    print("Generated discovery.yml does not Meltano discovery.yml")
+    print("Generated discovery.yml does not match Meltano discovery.yml")
     pprint(ddiff, indent = 2)
     sys.exit(1)
