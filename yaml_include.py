a7a7012d7992797415a8b11400585855543c00e6
AJ Steers <4809760-aaronsteers@users.noreply.gitlab.com>
2021-09-23 17:57:51 +0000

    Improved Yaml generation (Ruamel)

--- yaml_include.py
+++ yaml_include.py
@@ -1,20 +1,23 @@
 import json
 import os
-import yaml
+from ruamel.yaml import YAML
 from copy import deepcopy
+
+yaml = YAML()
 
 
 def merge(a, b, path=None, update=True):
     "http://stackoverflow.com/questions/7204805/python-dictionaries-of-dictionaries-merge"
     "merges b into a"
-    if path is None: path = []
+    if path is None:
+        path = []
     for key in b:
         if key in a:
 
             if isinstance(a[key], dict) and isinstance(b[key], dict):
                 merge(a[key], b[key], path + [str(key)])
             elif a[key] == b[key]:
-                pass # same leaf value
+                pass  # same leaf value
             elif isinstance(a[key], list) and isinstance(b[key], list):
                 if a[key] == "variants" and b[key] == "variants":
                     for idx, val in enumerate(b[key]):
@@ -23,13 +26,23 @@ def merge(a, b, path=None, update=True):
                             a_name = a[key][index].get("name")
                             if b_name == a_name:
                                 try:
-                                    a[key][idx] = merge(a[key][idx], b[key][idx], path + [str(key), str(idx)], update=update)
+                                    a[key][idx] = merge(
+                                        a[key][idx],
+                                        b[key][idx],
+                                        path + [str(key), str(idx)],
+                                        update=update,
+                                    )
                                 except IndexError:
                                     a[key].append(b[key][idx])
                 else:
                     for idx, val in enumerate(b[key]):
                         try:
-                            a[key][idx] = merge(a[key][idx], b[key][idx], path + [str(key), str(idx)], update=update)
+                            a[key][idx] = merge(
+                                a[key][idx],
+                                b[key][idx],
+                                path + [str(key), str(idx)],
+                                update=update,
+                            )
                         except IndexError:
                             a[key].append(b[key][idx])
             elif update:
@@ -37,24 +50,24 @@ def merge(a, b, path=None, update=True):
             elif not update:
                 pass
             else:
-                raise Exception('Conflict at %s' % '.'.join(path + [str(key)]))
+                raise Exception("Conflict at %s" % ".".join(path + [str(key)]))
         else:
             a[key] = b[key]
     return a
 
 
 with open("_data/scraped.yml", "r") as scraped_file:
-    scraped = yaml.load(scraped_file, Loader=yaml.FullLoader)
+    scraped = yaml.load(scraped_file)
 
-directory = '_data/singer/'
- 
+directory = "_data/singer/"
+
 for filename in os.listdir(directory):
     f = os.path.join(directory, filename)
     print(f)
     plugin_name = filename.split(".")[0]
 
     with open(f, "r") as plugin_file:
-        plugin_data = yaml.load(plugin_file, Loader=yaml.FullLoader)
+        plugin_data = yaml.load(plugin_file)
 
     includes = plugin_data.get("include").split("/")
     include_source = includes[0]
@@ -67,4 +80,3 @@ for filename in os.listdir(directory):
 
         with open(f"_data/taps/{plugin_name}.yml", "w") as outfile:
             yaml.dump(plugin_updated, outfile, default_flow_style=False)
-
