1336475df1adcd75f1409d7a84693c409d8a8b94
Taylor A. Murphy <taylor@meltano.com>
2022-06-06 14:37:49 -0500

    Update transformer, orchestrator, utilities, and files pages to be built from YAML (#550)
    * Update Singer-specific pages to be Meltano-neutral
    
    * Fix variant URLs
    
    * Cleanup
    
    * Sort default variants file
    
    * Update wrong reference
    
    * Fix links to extractors page
    
    * Add missing equality
    
    * Content and link tweaks
    
    * Fix Title and Navbar for Singer plugins
    
    * Move is_singer logic
    
    * Add meltano_type
    
    * Update missing plugin maintainers and labels
    
    * Update plugin labels
    
    * Fix URLs
    
    * Extraneous setting from merge conflict
    
    * default cleanup from is_singer adjustment
    
    * Fix comment and URL at bottom of page
    
    * Remove conflict indicator
    
    * remove conflict indicator
    
    * Update maintainers
    
    * Update plugin definitions
    
    * Move page topic and next steps into Ruby
    
    * Add settings preamble
    
    * Update JSON Schema
    
    * Remove markdown files
    
    * Update files maintenance status
    
    * Fix tap and target intros
    
    Co-authored-by: Douwe Maan <douwe@selenight.nl>

--- _plugins/meltano_plugin_page_generator.rb
+++ _plugins/meltano_plugin_page_generator.rb
@@ -60,28 +60,36 @@ class MeltanoPluginPageGenerator < Jekyll::Generator
       @site = site
       @base = site.source
       if plugin_type == 'tap'
+        singular_plugin_type = plugin_type
         plural_plugin_type = "taps"
-        @dir  = plural_plugin_type 
+        @dir  = plural_plugin_type
         variant_definition = Marshal.load(Marshal.dump(variant_definition))
         variant_definition['url'] = "/taps/#{plugin_name}"
         title = "#{variant_definition['label']} Singer #{plugin_type}"
         is_singer = true
         meltano_type = "extractors"
       elsif plugin_type == 'target'
+        singular_plugin_type = plugin_type
         plural_plugin_type   = "targets"
-        @dir  = plural_plugin_type 
+        @dir  = plural_plugin_type
         variant_definition = Marshal.load(Marshal.dump(variant_definition))
         variant_definition['url'] = "/targets/#{plugin_name}"
         title = "#{variant_definition['label']} Singer #{plugin_type}"
         is_singer = true
         meltano_type = "loaders"
       else
+        singular_plugin_type = plugin_type
+
         plural_plugin_type = "#{plugin_type}s"
         @dir  = plural_plugin_type
         title = "#{variant_definition['label']} Meltano #{plugin_type}"
+        if plugin_type == "utilitie"
+          singular_plugin_type = "utility"
+          title = "#{variant_definition['label']} Meltano #{singular_plugin_type}"
+        end
         is_singer = false
         meltano_type = plural_plugin_type
-      end   
+      end
 
       basename = plugin_name
       if variant_specific
@@ -89,12 +97,49 @@ class MeltanoPluginPageGenerator < Jekyll::Generator
         title += " (#{variant_definition['variant']} variant)"
       end
 
-      description =
-        if plugin_type == 'extractor'
-          "The open source Meltano extractor pulls data from #{variant_definition['label']} that can then be sent to a destination using a loader."
-        else
-          "The open source Meltano loader sends data into #{variant_definition['label']} after it was pulled from a source using an extractor."
-        end
+      if plugin_type == 'extractor'
+        page_topic = "The `#{variant_definition['name']}` [Meltano extractor](https://docs.meltano.com/concepts/plugins#extractors) pulls data from [#{variant_definition['label']}](#{variant_definition['domain_url']}) that can then be sent to a destination using a [loader](/loaders)."
+        next_steps = """Follow the remaining steps of the [Getting Started guide](https://docs.meltano.com/getting-started.html):
+
+1. [Select entities and attributes to extract](https://docs.meltano.com/getting-started.html#select-entities-and-attributes-to-extract)
+2. [Add a loader to send data to a destination](https://docs.meltano.com/getting-started.html#add-a-loader-to-send-data-to-a-destination)
+3. [Run a data integration (EL) pipeline](https://docs.meltano.com/getting-started.html#run-a-data-integration-el-pipeline)
+
+If you run into any issues, [learn how to get help](#looking-for-help)."""
+      elsif plugin_type == 'loader'
+        page_topic = "The `#{variant_definition['name']}` [Meltano loader](https://docs.meltano.com/concepts/plugins#loaders) sends data into [#{variant_definition['label']}](#{variant_definition['domain_url']}) after it was pulled from a source using an [extractor](/extractors)."
+        next_steps = """Follow the remaining steps of the [Getting Started guide](https://docs.meltano.com/getting-started.html):
+
+1. [Run a data integration (EL) pipeline](https://docs.meltano.com/getting-started.html#run-a-data-integration-el-pipeline)
+
+If you run into any issues, [learn how to get help](#looking-for-help)."""
+      elsif plugin_type == 'tap'
+        page_topic = "The `#{variant_definition['name']}` [Singer tap](/singer/taps) pulls data from [#{variant_definition['label']}](#{variant_definition['domain_url']}) that can then be sent to a destination using a [Singer target](/singer/targets)."
+      elsif plugin_type == 'target'
+        page_topic = "The `#{variant_definition['name']}` [Singer target](/singer/targets) sends data into [#{variant_definition['label']}](#{variant_definition['domain_url']}) after it was pulled from a source using a [Singer tap](/singer/taps)."
+      elsif plugin_type == 'transformer'
+        page_topic = "The `#{variant_definition['name']}` [transformer](https://docs.meltano.com/guide/transformation) uses SQL to transform data stored in your warehouse."
+        next_steps = """Follow the remaining steps of the [Getting Started guide](https://docs.meltano.com/getting-started.html):
+
+1. [Transform loaded data for analysis](https://docs.meltano.com/getting-started.html#transform-loaded-data-for-analysis)
+
+If you run into any issues, [learn how to get help](#looking-for-help)."""
+      elsif plugin_type == 'orchestrator'
+        page_topic = "The `#{variant_definition['name']}` [orchestrator](https://docs.meltano.com/concepts/plugins#orchestrators) allows for workflows to be programmatically authored, scheduled, and monitored."
+        next_steps = """1. Use the [meltano schedule](https://docs.meltano.com/reference/command-line-interface#schedule) command to create pipeline schedules in your project, to be run by Airflow.
+2. Start Scheduler and Webserver or execute Airflow commands directly using the instructions in [the Meltano docs](https://docs.meltano.com/guide/orchestration#starting-the-airflow-scheduler).
+
+If you run into any issues, [learn how to get help](#looking-for-help).
+"""
+      elsif plugin_type == 'file'
+        page_topic = "The `#{variant_definition['name']}` [file bundle](https://docs.meltano.com/concepts/plugins#file-bundles) #{variant_definition['definition']}"
+        next_steps = "#{variant_definition['next_steps']}"
+      elsif plugin_type == 'utilitie'
+        page_topic = "The `#{variant_definition['name']}` [utility](https://docs.meltano.com/concepts/plugins#utilities) #{variant_definition['definition']}"
+        next_steps = "#{variant_definition['next_steps']}"
+      else
+        page_topic = "This is the default text."
+      end
 
       @basename = basename
       @ext      = '.html'
@@ -102,15 +147,17 @@ class MeltanoPluginPageGenerator < Jekyll::Generator
 
       @data = {
         'title' => title,
-        'description' => description,
+        'page_topic' => page_topic,
         'image_url' => variant_definition['logo_url'],
         'variant_specific' => variant_specific,
         'plugin_type' => plugin_type,
+        'singular_plugin_type' => singular_plugin_type,
         'plural_plugin_type' => plural_plugin_type,
         'plugin' => variant_definition,
         'plugin_name_no_prefix' => plugin_name,
         'is_singer' => is_singer,
-        'meltano_type' => meltano_type
+        'meltano_type' => meltano_type,
+        'next_steps' => next_steps
       }
 
       data.default_proc = proc do |_, key|
