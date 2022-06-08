557d1c3a35ec74671c7099a976022e5ef08c64fa
Douwe Maan <douwe@selenight.nl>
2022-06-07 18:43:49 -0700

    Document Superset dependencies, configuration, and next steps

--- _plugins/meltano_plugin_page_generator.rb
+++ _plugins/meltano_plugin_page_generator.rb
@@ -100,16 +100,12 @@ class MeltanoPluginPageGenerator < Jekyll::Generator
 
 1. [Select entities and attributes to extract](https://docs.meltano.com/getting-started.html#select-entities-and-attributes-to-extract)
 2. [Add a loader to send data to a destination](https://docs.meltano.com/getting-started.html#add-a-loader-to-send-data-to-a-destination)
-3. [Run a data integration (EL) pipeline](https://docs.meltano.com/getting-started.html#run-a-data-integration-el-pipeline)
-
-If you run into any issues, [learn how to get help](#looking-for-help)."""
+3. [Run a data integration (EL) pipeline](https://docs.meltano.com/getting-started.html#run-a-data-integration-el-pipeline)"""
       elsif plugin_type == 'loader'
         page_topic = "The `#{variant_definition['name']}` [Meltano loader](https://docs.meltano.com/concepts/plugins#loaders) sends data into [#{variant_definition['label']}](#{variant_definition['domain_url']}) after it was pulled from a source using an [extractor](/extractors)."
         next_steps = """Follow the remaining steps of the [Getting Started guide](https://docs.meltano.com/getting-started.html):
 
-1. [Run a data integration (EL) pipeline](https://docs.meltano.com/getting-started.html#run-a-data-integration-el-pipeline)
-
-If you run into any issues, [learn how to get help](#looking-for-help)."""
+1. [Run a data integration (EL) pipeline](https://docs.meltano.com/getting-started.html#run-a-data-integration-el-pipeline)"""
       elsif plugin_type == 'tap'
         page_topic = "The `#{variant_definition['name']}` [Singer tap](/singer/taps) pulls data from [#{variant_definition['label']}](#{variant_definition['domain_url']}) that can then be sent to a destination using a [Singer target](/singer/targets)."
       elsif plugin_type == 'target'
@@ -118,22 +114,18 @@ If you run into any issues, [learn how to get help](#looking-for-help)."""
         page_topic = "The `#{variant_definition['name']}` [transformer](https://docs.meltano.com/guide/transformation) uses SQL to transform data stored in your warehouse."
         next_steps = """Follow the remaining steps of the [Getting Started guide](https://docs.meltano.com/getting-started.html):
 
-1. [Transform loaded data for analysis](https://docs.meltano.com/getting-started.html#transform-loaded-data-for-analysis)
-
-If you run into any issues, [learn how to get help](#looking-for-help)."""
+1. [Transform loaded data for analysis](https://docs.meltano.com/getting-started.html#transform-loaded-data-for-analysis)"""
       elsif plugin_type == 'orchestrator'
         page_topic = "The `#{variant_definition['name']}` [orchestrator](https://docs.meltano.com/concepts/plugins#orchestrators) allows for workflows to be programmatically authored, scheduled, and monitored."
         next_steps = """1. Use the [meltano schedule](https://docs.meltano.com/reference/command-line-interface#schedule) command to create pipeline schedules in your project, to be run by Airflow.
 2. Start Scheduler and Webserver or execute Airflow commands directly using the instructions in [the Meltano docs](https://docs.meltano.com/guide/orchestration#starting-the-airflow-scheduler).
-
-If you run into any issues, [learn how to get help](#looking-for-help).
 """
       elsif plugin_type == 'file'
         page_topic = "The `#{variant_definition['name']}` [file bundle](https://docs.meltano.com/concepts/plugins#file-bundles) #{variant_definition['definition']}"
-        next_steps = "#{variant_definition['next_steps']}"
+        next_steps = variant_definition['next_steps']
       elsif plugin_type == 'utility'
         page_topic = "The `#{variant_definition['name']}` [utility](https://docs.meltano.com/concepts/plugins#utilities) #{variant_definition['definition']}"
-        next_steps = "#{variant_definition['next_steps']}"
+        next_steps = variant_definition['next_steps']
       else
         page_topic = "`#{variant_definition['name']}` #{variant_definition['definition']}"
       end
