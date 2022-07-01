92a021e31653eacffabd33224754f4c0544a14af
Reina Feather <feather.rw@gmail.com>
2022-07-01 17:33:34 -0500

    Move into shell script, use AWS credentials file
    (ran into the same problem as this blog post:
    https://remysharp.com/2019/05/18/aws-inside-netlify)

--- Makefile
+++
@@ -1,7 +0,0 @@
-bundle:
-	curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" && \
-    unzip -q awscliv2.zip && \
-    ./aws/install -i ~/aws-cli -b ~/aws-cli/bin && \
-    ~/aws-cli/bin/aws s3 cp s3://prod-meltano-bucket-01/hub_metrics/metrics.yml ./_data/metrics.yml && \
-    ~/aws-cli/bin/aws s3 cp s3://prod-meltano-bucket-01/hub_metrics/audit.yml ./_data/audit.yml
-	bundle exec jekyll build --config _config.yml,_config_netlify.yml
---
+++ build_with_metrics.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+# Run a jekyll build in netlify, but grab metrics stored in s3 first
+
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip" 
+unzip -q awscliv2.zip 
+./aws/install -i ~/aws-cli -b ~/aws-cli/bin 
+cat > ./aws <<EOL
+[default]
+region = $ENV_DEFAULT_REGION
+aws_secret_access_key = $ENV_SECRET_ACCESS_KEY
+aws_access_key_id = $ENV_ACCESS_KEY_ID
+EOL
+~/aws-cli/bin/aws s3 cp s3://prod-meltano-bucket-01/hub_metrics/metrics.yml ./_data/metrics.yml 
+~/aws-cli/bin/aws s3 cp s3://prod-meltano-bucket-01/hub_metrics/audit.yml ./_data/audit.yml
+bundle exec jekyll build --config _config.yml,_config_netlify.yml
