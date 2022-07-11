78b668ea2fe7580fea8bb0d69088c71af466ba9e
Reina Feather <reina@meltano.com>
2022-07-11 13:55:41 -0500

    Add initial search functionality (#662)
    * Add initial search functionality
    
    * Fix whitespacing
    
    * Tweak search results styling

--- hub-2.0/ui/src/components/Search.vue
+++ hub-2.0/ui/src/components/Search.vue
@@ -1,68 +1,155 @@
 <template>
-
-<div>
-  <div class="search">
-      <input type="text" name="search" id="search" placeholder="Search for a plugin..." class="search-bar" v-model="search" />
+  <div>
+    <div class="search">
+      <input
+        type="text"
+        name="search"
+        id="search"
+        placeholder="Search for a plugin..."
+        class="search-bar"
+        v-model="search"
+      />
       <button class="search-button">Search</button>
-  </div>
-<div class="results">
-  <!-- <p>results!</p> -->
-      <!-- <div v-if="searchResults.length > 0">
-        <article v-for="extractor in searchResults" :key="extractor.node.id">
-          <h1><g-link :to="extractor.node.path">{{ extractor.node.name }}</g-link></h1>
-        </article>
+      <div class="results" v-if="search != ''">
+        <div v-if="searchResults.length > 0">
+          <article
+            v-for="plugin in searchResults.slice(0, 10)"
+            :key="plugin.node.id"
+            class="result-item"
+          >
+            <g-link :to="plugin.node.path">
+              <h1>
+                {{ plugin.node.label }}
+              </h1>
+
+              <span>{{ plugin.node.description || "No description" }}</span>
+              <span class="plugin-type">{{
+                plugin.node.pluginType
+              }}</span></g-link
+            >
+          </article>
+        </div>
+        <div v-else>
+          <p>Your search didn't return any results. Please try again.</p>
+        </div>
+      </div>
     </div>
-    <div v-else>
-      <p>Your search didn't return any results. Please try again.</p>
-    </div> -->
-</div>
-</div>
+  </div>
 </template>
-
-data() {
-
-}
 
 <script>
 export default {
-  name: 'Search',
+  name: "Search",
   data() {
     return {
-      search: ''
-    }
+      search: "",
+    };
   },
   computed: {
     searchResults() {
-      // return this.$static.allExtractors.edges.filter(extractor => {
-      //   return extractor.node.name.toLowerCase().includes(this.search.toLowerCase().trim())
-      // })
-    }
-  }
-}
+      const pluginCollections = [
+        this.$static.allExtractors,
+        this.$static.allLoaders,
+        this.$static.allFiles,
+        this.$static.allOrchestrators,
+        this.$static.allUtilities,
+        this.$static.allTransformers,
+      ];
+      console.log(pluginCollections);
+      return pluginCollections.flatMap((coll) => {
+        return coll.edges.filter((plugin) => {
+          const pluginText =
+            plugin.node.name + plugin.node.description + plugin.node.label;
+          return pluginText
+            .toLowerCase()
+            .includes(this.search.toLowerCase().trim());
+        });
+      });
+    },
+  },
+};
 </script>
 
-<page-query>
+<static-query>
 query {
-	allExtractors{
-		edges {
+  allExtractors (filter: {isDefault: {eq: true}}){
+    edges {
       node {
         id
-				description
+        description
         path
-				label
-				name
-				logo_url
-				namespace
-				variant
-				pip_url
-				repo
-				maintenance_status
-				keywords
+        label
+        name
+        logo_url
+        pluginType
       }
     }
   }
+  allLoaders (filter: {isDefault: {eq: true}}){
+    edges {
+      node {
+        id
+        description
+        path
+        label
+        name
+        logo_url
+        pluginType
+      }
+    }
+  }
+  allOrchestrators (filter: {isDefault: {eq: true}}){
+    edges {
+      node {
+        id
+        path
+        label
+        name
+        logo_url
+        pluginType
+      }
+    }
+  }
+  allTransformers (filter: {isDefault: {eq: true}}){
+    edges {
+      node {
+        id
+        path
+        label
+        name
+        logo_url
+        pluginType
+      }
+    }
+  }
+  allUtilities (filter: {isDefault: {eq: true}}){
+    edges {
+      node {
+        id
+        description
+        path
+        label
+        name
+        logo_url
+        pluginType
+      }
+    }
+  }
+  allFiles (filter: {isDefault: {eq: true}}){
+    edges {
+      node {
+        id
+        path
+        label
+        name
+        logo_url
+        pluginType
+      }
+    }
+  }
+  
 }
-</page-query>
+</static-query>
 
 <style lang="scss">
 .search {
@@ -81,13 +168,34 @@ query {
     padding-left: 20px;
   }
 
-
   .search-button {
     background: #d9042b;
     border: none;
     color: #fff;
     padding: 20px;
     font-size: 16px;
+  }
+
+  .results {
+    // display: none;
+    box-shadow: 1px 2px 4px rgba(0, 0, 0, 0.3);
+    position: absolute;
+    width: 75%;
+    align-content: left;
+    margin-top: 80px;
+    background: white;
+    line-height: 1rem;
+    .result-item {
+      color: black;
+      padding: 0.5rem;
+      border-bottom: 2px solid black;
+      a {
+        text-decoration: none;
+      }
+      .plugin-type {
+        float: right;
+      }
+    }
   }
 }
 </style>
