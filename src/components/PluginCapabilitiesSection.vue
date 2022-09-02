83417393dc65e3f8d778d415bb4c20ba525f506c
alexmarple <alexmarple@gmail.com>
2022-09-02 11:01:28 -0700

    Replace <p>'s with <span>'s to conform to prettier standards

--- src/components/PluginCapabilitiesSection.vue
+++ src/components/PluginCapabilitiesSection.vue
@@ -2,11 +2,14 @@
   <div>
     <h2 id="capabilities">Capabilities</h2>
     <span v-if="capabilities">
-      <p>The current capabilities for
-      <pre class="inline-code-block"><code>{{ name }}</code></pre> may have been automatically set when originally added
-      to the Hub. Please review the capabilities when using this {{ plugin_type }}. If you find they are out of date,
-      please consider updating them by making a pull request to the YAML file that defines the capabilities for this
-      {{ plugin_type }}.</p>
+      <span
+        >The current capabilities for
+        <pre class="inline-code-block"><code>{{ name }}</code></pre>
+        may have been automatically set when originally added to the Hub. Please review the
+        capabilities when using this {{ plugin_type }}. If you find they are out of date, please
+        consider updating them by making a pull request to the YAML file that defines the
+        capabilities for this {{ plugin_type }}.</span
+      >
       <p>This plugin has the following capabilities:</p>
       <ul>
         <li v-for="(capability, index) in $page.extractors.capabilities" v-bind:key="index">
@@ -15,23 +18,25 @@
       </ul>
       <p>
         You can
-        <a href="https://docs.meltano.com/guide/configuration#overriding-discoverable-plugin-properties">override these
-          capabilities or specify additional ones</a>
+        <a
+          href="https://docs.meltano.com/guide/configuration#overriding-discoverable-plugin-properties"
+          >override these capabilities or specify additional ones</a
+        >
         in your <code>meltano.yml</code> by adding the <code>capabilities</code> key.
       </p>
     </span>
-    <span v-else>This plugin currently has no capabilities defined. If you know the capabilities required by this
-      plugin, <a href="#contribute">please contribute!</a></span>
+    <span v-else
+      >This plugin currently has no capabilities defined. If you know the capabilities required by
+      this plugin, <a href="#contribute">please contribute!</a></span
+    >
   </div>
 </template>
-  
-  <script>
+
+<script>
 export default {
   name: "PluginCapabilitiesSection",
   props: ["capabilities", "name", "plugin_type"],
 };
 </script>
-  
-  <style>
-  </style>
-  
+
+<style></style>
--- src/components/PluginCommandsSection.vue
+++ src/components/PluginCommandsSection.vue
@@ -1,36 +1,41 @@
 <template>
-    <div v-if="commands">
-        <h2 id="commands">Commands</h2>
-        <p>The {{ name }} {{ plugin_type }} supports the following commands that can be used with
-        <pre><code>meltano invoke</code></pre>:</p>
-        <div v-for="(key, value, index) in commands" v-bind:key="index">
-            <span v-if="value">
-                <h3>
-                    <pre><code>{{ key.name }}</code></pre>
-                </h3>
-                <p>Equivalent to:
-                <pre><code>{{ key.args }}</code></pre>
-                </p>
-                <p>{{ key.description }}</p>
-                <h3>How to use</h3>
-                <p>Run this command using
-                <pre><code>meltano invoke</code></pre>
-                </p>
-                <pre><code>meltano invoke {{ name }}:thing-to-do [additional arguments...]</code></pre>
-            </span>
-        </div>
+  <div v-if="commands">
+    <h2 id="commands">Commands</h2>
+    <span
+      >The {{ name }} {{ plugin_type }} supports the following commands that can be used with
+      <pre><code>meltano invoke</code></pre>
+      :</span
+    >
+    <div v-for="(key, value, index) in commands" v-bind:key="index">
+      <span v-if="value">
+        <h3>
+          <pre><code>{{ key.name }}</code></pre>
+        </h3>
+        <span
+          >Equivalent to:
+          <pre><code>{{ key.args }}</code></pre>
+        </span>
+        <p>{{ key.description }}</p>
+        <h3>How to use</h3>
+        <span
+          >Run this command using
+          <pre><code>meltano invoke</code></pre>
+        </span>
+        <pre><code>meltano invoke {{ name }}:thing-to-do [additional arguments...]</code></pre>
+      </span>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "PluginCommandsSection",
-    props: ["commands", "name", "plugin_type"],
+  name: "PluginCommandsSection",
+  props: ["commands", "name", "plugin_type"],
 };
 </script>
 
 <style>
 pre {
-    display: inline-block
+  display: inline-block;
 }
 </style>
