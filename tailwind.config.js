429cd2fae37832128959a154d79f1c09c947b181
Reina Feather <reina@meltano.com>
2022-09-13 22:24:42 -0500

    Disable Tailwind Preflight

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
   content: [
     "./src/components/**/*.{js,vue,ts}",
     "./src/layouts/**/*.vue",
