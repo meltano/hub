0d39d2bf1daeb5bbafa42a2b5f4cb7fd8bb83ee9
Reina Feather <reina@meltano.com>
2022-09-15 13:54:26 -0500

    Add safelist config for development

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,10 @@ module.exports = {
     "./src/pages/**/*.vue",
     "./src/templates/**/*.vue",
   ],
+  // Safelist is a brute force method to get tailwind to work with `gridsome develop`
+  // https://tailwindcss.com/docs/content-configuration#safelisting-classes
+  // Only enable during local dev
+  // safelist: [{ pattern: /.*/ }],
   theme: {
     extend: {},
   },
