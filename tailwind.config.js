520740bc11e21ab0b820d97f63aae300862fc73d
Reina Feather <reina@meltano.com>
2022-09-21 19:07:40 -0500

    Add anchor links to singer page headings

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
     "./src/layouts/**/*.vue",
     "./src/pages/**/*.vue",
     "./src/templates/**/*.vue",
+    "./gridsome.config.js",
   ],
   // Safelist is a brute force method to get tailwind to work with `gridsome develop`
   // https://tailwindcss.com/docs/content-configuration#safelisting-classes
