61304377257fa454fec4f43558d6d81ecaccefb1
Aaron ("AJ") Steers <aj@meltano.com>
2022-09-28 15:47:31 -0700

    feat(gridsome): add config instructions to detail pages

--- src/components/PluginSettingsSection.vue
+++ src/components/PluginSettingsSection.vue
@@ -5,8 +5,9 @@
       <div class="prose mt-3 p-2" v-if="preamble" v-html="preamble"></div>
       <p>
         The
-        <code>{{ name }}</code> settings that are known to Meltano are documented below. To quickly
-        find the setting you're looking for, click on any setting name from the list:
+        <code>{{ name }}</code> settings that are known to Meltano are
+        documented below. To quickly find the setting you're looking for, click
+        on any setting name from the list:
       </p>
       <ul class="list-disc list-inside pl-4">
         <li v-for="(setting, index) in settings" v-bind:key="index">
@@ -21,17 +22,32 @@
           href="https://docs.meltano.com/guide/configuration#overriding-discoverable-plugin-properties"
           >override these settings or specify additional ones</a
         >
-        in your <code>meltano.yml</code> by adding the <code>settings</code> key.
+        in your <code>meltano.yml</code> by adding the
+        <code>settings</code> key.
       </p>
       <p>
-        Please consider adding any settings you have defined locally to this definition on
-        MeltanoHub by making a pull request to the
+        Please consider adding any settings you have defined locally to this
+        definition on MeltanoHub by making a pull request to the
         <a
           :href="`https://github.com/meltano/hub/blob/main/_data/meltano/${plugin_type_plural}/${name}/${variant}.yml`"
         >
           YAML file</a
         >
         that defines the settings for this plugin.
+      </p>
+      <p class="text-3xl py-4" id="configuration">Configuration</p>
+      <p>
+        Settings for {{ plugin.name }} can be
+        <a
+          href="https://docs.meltano.com/reference/command-line-interface#config"
+          >configured</a
+        >
+        by running <code>meltano config {{ plugin.name }} set --interactive</code>,
+        or by using
+        <a
+          href="https://docs.meltano.com/guide/configuration#configuring-settings"
+          >environment variables</a
+        >.
       </p>
       <span v-for="(setting, index) in settings" v-bind:key="index">
         <p class="text-xl mt-3" :id="setting.name + '-setting'">
@@ -46,8 +62,9 @@
       </span>
     </span>
     <span v-else
-      >This plugin currently has no settings defined. If you know the settings required by this
-      plugin, <a href="#contribute">please contribute!</a></span
+      >This plugin currently has no settings defined. If you know the settings
+      required by this plugin,
+      <a href="#contribute">please contribute!</a></span
     >
   </div>
 </template>
