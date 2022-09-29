d8e08cbc6fe144e93cb4f1ae2a11b595a6af1e59
Reina Feather <reina@meltano.com>
2022-09-28 19:29:57 -0500

    Add setting environment vars and default values

--- src/components/PluginSettingsSection.vue
+++ src/components/PluginSettingsSection.vue
@@ -33,12 +33,25 @@
         >
         that defines the settings for this plugin.
       </p>
-      <span v-for="(setting, index) in settings" v-bind:key="index">
-        <p class="text-xl mt-3" :id="setting.name + '-setting'">
+      <span class="mt-6" v-for="(setting, index) in settings" v-bind:key="index">
+        <p class="mt-3 text-xl" :id="setting.name + '-setting'">
           <code>{{ setting.label }} ({{ setting.name }})</code>
         </p>
+        <ul class="list-inside list-disc pl-4 text-sm">
+          <li>
+            Environment variable:
+            <code>{{
+              `${name.replaceAll("-", "_").toUpperCase()}_${setting.name
+                .replaceAll(".", "_")
+                .toUpperCase()}`
+            }}</code>
+          </li>
+          <li v-if="setting.value">
+            Default Value: <code class="overflow-x-scroll">{{ setting.value }}</code>
+          </li>
+        </ul>
         <div
-          class="prose bg-slate-100 p-2"
+          class="prose mt-3 bg-slate-100 p-2"
           v-if="setting.description"
           v-html="setting.description_rendered"
         ></div>
