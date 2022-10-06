c04d94822eb871e900e6698ed0d1040633f164c5
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2022-10-06 14:06:31 -0500

    Make `prettier` happy

--- src/templates/Plugins.vue
+++ src/templates/Plugins.vue
@@ -94,7 +94,9 @@
                   <li>
                     Test that extractor settings are valid using
                     <a href="https://docs.meltano.com/reference/command-line-interface#test">
-                      <pre class="inline-code-block"><code>meltano config {{ $page.plugins.name }} test</code></pre>
+                      <pre
+                        class="inline-code-block"
+                      ><code>meltano config {{ $page.plugins.name }} test</code></pre>
                     </a>
                     :
                   </li>
