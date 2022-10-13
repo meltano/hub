9e90196dc841e73ea29a7839e33ec1cacf0efeaf
Will Da Silva <will@willdasilva.xyz>
2022-10-13 16:18:05 -0400

    feat: Make logos link to homepages
    Closes #903

--- src/templates/Plugins.vue
+++ src/templates/Plugins.vue
@@ -6,6 +6,7 @@
           <table>
             <tr>
               <td style="padding: 25px">
+              <a v-if="domain_url" href="domain_url" target="_blank" rel="noopener noreferrer">
                 <g-image
                   v-if="$page.plugins.logo_url"
                   :src="
@@ -15,6 +16,18 @@
                     )}`)
                   "
                 />
+              </a>
+              <div v-else>
+                <g-image
+                  v-if="$page.plugins.logo_url"
+                  :src="
+                    require(`!!assets-loader?width=250&height=200&fit=inside!@logos/${$page.plugins.logo_url.replace(
+                      '/assets/logos/',
+                      ''
+                    )}`)
+                  "
+                />
+              </div>
               </td>
               <td>
                 <p class="text-3xl py-8">
