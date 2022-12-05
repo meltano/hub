e505766d7d3fdbcc754561a22c3cee97f0c328ce
pnadolny13 <patnadolny@gmail.com>
2022-12-05 15:22:05 -0500

    fix prettier ci errors

--- src/pages/Extractors.vue
+++ src/pages/Extractors.vue
@@ -8,7 +8,9 @@
       </p>
       <p>
         To learn more about extracting and loading data using Meltano, refer to the
-        <a href="https://docs.meltano.com/getting-started/part1" target="_blank">Getting Started guide</a>.
+        <a href="https://docs.meltano.com/getting-started/part1" target="_blank"
+          >Getting Started guide</a
+        >.
       </p>
       <div
         class="grid grid-cols-2 md:grid-cols-4 bg-slate-200 rounded-lg p-4 mt-4 md:m-4 gap-4 w-full place-items-stretch"
