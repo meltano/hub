186be8d98f742d7961984fd4a6802fbcd4bf2eea
Taylor A. Murphy <taylor@meltano.com>
2023-03-08 14:10:01 -0600

    add link to anchor

--- src/templates/Plugins.vue
+++ src/templates/Plugins.vue
@@ -275,7 +275,7 @@
               <div v-else></div>
 
               <!-- Help section -->
-              <p>If you run into any issues, learn how to get help.</p>
+              <p>If you run into any issues, <a href="#looking-for-help">learn how to get help</a>.</p>
             </div>
             <div>
               <PluginCapabilitiesSection
