9855b80d336edb5d488a55509d833020e3ca1dfa
Andrii S <andriisharapatov@gmail.com>
2023-03-23 14:41:46 +0200

    border radius

--- src/layouts/Header.vue
+++ src/layouts/Header.vue
@@ -164,6 +164,7 @@ export default {
 .dropdown-content {
   display: none;
   position: absolute;
+  border-radius: 5px;
   // overflow: auto;
   box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.2);
   text-align: center;
