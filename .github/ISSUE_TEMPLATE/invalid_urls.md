1ce7148ecac3fe1fc92c56cb68a744a961df4454
pnadolny13 <patnadolny@gmail.com>
2023-05-08 15:39:33 -0400

    add action to check links

---
+++ .github/ISSUE_TEMPLATE/invalid_urls.md
@@ -0,0 +1,5 @@
+---
+title: 'chore: [Hub Bot] Invalid URLs Found'
+---
+
+{{ env.INVALID_URLS }}
---
+++ utility_scripts/plugin_definitions/check_links.py
@@ -0,0 +1,52 @@
+import json
+import os
+import re
+
+import requests
+
+EXCLUDED_FILES = [
+    "_data/scraped.yml",
+    "_data/audit.yml",
+    "_data/default_variants.yml",
+    "_data/maintainers.yml",
+    "_data/singer_airbyte_map.yml",
+    "_data/variant_metrics.yml",
+]
+
+def test_urls(yml_path):
+    print(f"Checking: {yml_path}")
+    with open(yml_path, "r") as plugin_file:
+        bad_urls = []
+        plugin_data = plugin_file.read()
+        all_urls = re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', plugin_data)
+        urls = {url.rstrip('.git') for url in all_urls}
+        for url in urls:
+            resp = requests.get(url)
+            if resp.status_code != 200:
+                bad_urls.append(url)
+        return bad_urls
+
+
+def find_all_yamls(f_path="_data/", INCLUDED_FILES=None):
+    for root, subdirs, files in os.walk(f_path):
+        for file in files:
+            relative_path = os.path.join(root, file)
+            if file.endswith(".yml") and relative_path not in EXCLUDED_FILES and "angaza" in file:
+                yield relative_path
+        for subdir in subdirs:
+            find_all_yamls(os.path.join(root, f_path, subdir))
+
+
+if __name__ == "__main__":
+    """
+    This script iterates all the plugin definition files and checks
+    their urls to make sure they are valid. Returning a dictionary
+    of bad links.
+    """
+    bad_links = {}
+    for yaml_file in find_all_yamls():
+        bad_urls = test_urls(yaml_file)
+        if bad_urls:
+            path = yaml_file.split("_data/meltano/")[1]
+            bad_links[f"https://github.com/meltano/hub/blob/main/_data/meltano/{path}"] = bad_urls
+    print(json.dumps(bad_links))
