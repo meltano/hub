b59e673e13a725bb0c229c8fc920f7babfa2a5bb
Wil <wilchow@gmail.com>
2023-07-28 12:55:46 -0500

    Updated Tailwind

--- gridsome.config.js
+++ gridsome.config.js
@@ -13,6 +13,7 @@ module.exports = {
 
       options: {
         tailwindConfig: "./tailwind.config.js",
+        shouldImport: true,
       },
     },
     {
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/components/**/*.{js,vue,ts}",
-    "./src/layouts/**/*.vue",
-    "./src/pages/**/*.vue",
-    "./src/templates/**/*.vue",
-    "./gridsome.config.js",
-  ],
+  content: ["./src/**/*.{js,vue,ts}"],
+  important: true,
   // Safelist is a brute force method to get tailwind to work with `gridsome develop`
   // https://tailwindcss.com/docs/content-configuration#safelisting-classes
   // Only enable during local dev
