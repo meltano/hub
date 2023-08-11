f8659a84536d04d64bc56bd38868f8e4d78e7187
Wil <wilchow@gmail.com>
2023-08-11 14:44:43 -0500

    Fixed prettier issue

--- src/components/home/HeroCard.vue
+++ src/components/home/HeroCard.vue
@@ -1,9 +1,13 @@
 <template>
   <a
     :href="item.url"
-    class="block text-white hover:bg-white/70 bg-purple/5 border border-purple/20 rounded group"
+    class="block text-white border rounded hover:bg-white/70 bg-purple/5 border-purple/20 group"
   >
-    <img :src="item.svg" :alt="item.name" class="lg:w-40 w-40 md:w-28 lg:h-40 h-40 md:h-28 mx-auto mt-12 md:mt-6 lg:mt-12 mb-8 md:mb-4 lg:mb-8 " />
+    <img
+      :src="item.svg"
+      :alt="item.name"
+      class="w-40 h-40 mx-auto mt-12 mb-8 lg:w-40 md:w-28 lg:h-40 md:h-28 md:mt-6 lg:mt-12 md:mb-4 lg:mb-8"
+    />
     <span class="flex flex-row justify-center mb-12 md:mb-6 lg:mb-12">
       <span
         class="bg-blue flex flex-row justify-center font-ibm items-center w-auto rounded-md px-4 py-2.5 border border-blue leading-tight group-hover:bg-purple group-hover:border-purple"
@@ -17,9 +21,9 @@
 
 <script>
 export default {
-  name: 'HeroCard',
+  name: "HeroCard",
   props: {
-    item: Object
-  }
-}
+    item: Object,
+  },
+};
 </script>
